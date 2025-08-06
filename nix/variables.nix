9046abc6d540826ccb8d8d1f198541314b937af4
Rich Schumacher <rich.schumacher@brivo.com>
2025-08-05 22:49:49 -0400

    chore: Bump versions for all the services
    * audiobookshelf
    * diun
    * gotify
    * grafana
    * homer
    * influxdb
    * jellyseer
    * mosquitto
    * postgres
    * prowlarr
    * radarr
    * smokeping
    * sonarr
    * tandoor
    * tunarr
    * unifi

--- nix/variables.nix
+++ nix/variables.nix
@@ -7,7 +7,7 @@
 
     audiobookshelf = {
       ip      = "192.168.20.234";
-      version = "2.16.2";
+      version = "2.27.0";
       uid     = 965;
     };
 
@@ -19,31 +19,31 @@
 
     diun = {
       ip      = "192.168.20.214";
-      version = "4.26.0";
+      version = "4.29.0";
       uid     = 976;
     };
 
     gotify = {
       ip      = "192.168.20.213";
-      version = "2.4.0";
+      version = "2.6.3";
       uid     = 987;
     };
 
     grafana = {
       ip      = "192.168.20.226";
-      version = "11.3.0";
+      version = "12.1.0";
       uid     = 971;
     };
 
     homer = {
       ip      = "192.168.20.223";
-      version = "v24.11.1";
+      version = "v24.12.1";
       uid     = 986;
     };
 
     influxdb = {
       ip      = "192.168.20.228";
-      version = "2.7.10";
+      version = "2.7.12";
       uid     = 970;
     };
 
@@ -51,6 +51,12 @@
       ip      = "192.168.20.218";
       version = "10.10.1";
       uid     = 985;
+    };
+
+    jellyseerr = {
+      ip      = "192.168.20.222";
+      version = "2.7.2";
+      uid     = 978;
     };
 
     minecraft-java = {
@@ -67,31 +73,25 @@
 
     mosquitto = {
       ip      = "192.168.20.212";
-      version = "2.0.20";
+      version = "2.0.22";
       uid     = 988;
-    };
-
-    jellyseerr = {
-      ip      = "192.168.20.222";
-      version = "2.0.1";
-      uid     = 978;
     };
 
     postgres16 = {
       ip      = "192.168.20.224";
-      version = "16";
+      version = "16.9";
       uid     = 983;
     };
 
     prowlarr = {
       ip      = "192.168.20.217";
-      version = "1.25.4";
+      version = "1.37.0";
       uid     = 989;
     };
 
     radarr = {
       ip      = "192.168.20.220";
-      version = "5.14.0";
+      version = "5.26.2";
       uid     = 982;
     };
 
@@ -102,19 +102,19 @@
 
     smokeping = {
       ip      = "192.168.20.211";
-      version = "2.8.2";
+      version = "2.9.0";
       uid     = 993;
     };
 
     sonarr = {
       ip      = "192.168.20.221";
-      version = "4.0.10";
+      version = "4.0.15";
       uid     = 981;
     };
 
     tandoor = {
       ip      = "192.168.20.233";
-      version = "1.5.20";
+      version = "1.5.35";
       uid     = 966;
     };
 
@@ -126,13 +126,13 @@
 
     tunarr = {
       ip      = "192.168.20.235";
-      version = "0.16.6-vaapi";
+      version = "0.20.6";
       uid     = 964;
     };
 
     unifi = {
       ip      = "192.168.10.10";
-      version = "8.6.9";
+      version = "9.3.45";
       uid     = 975;
     };
 
