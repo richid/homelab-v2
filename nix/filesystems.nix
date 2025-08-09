ff37fe3e71c92ebcd0ad1fc1b00297694a295f61
Rich Schumacher <rich.schu@gmail.com>
2025-08-08 23:05:18 -0400

    chore: Updates to work with NixOS 25.05 channel
    * Samba options have been moved: https://github.com/NixOS/nixpkgs/pull/302681
    * Remove redundant device labels: https://github.com/NixOS/nixpkgs/pull/362481
    * upsmon user is primary: https://github.com/NixOS/nixpkgs/pull/336599

--- nix/filesystems.nix
+++ nix/filesystems.nix
@@ -3,73 +3,61 @@
     "/mnt/app-data" = {
       device = "app-data";
       fsType = "zfs";
-      label  = "app-data";
     };
 
     "/mnt/dozer" = {
       device = "dozer";
       fsType = "zfs";
-      label  = "dozer";
     };
 
     "/mnt/parity0" = {
       device = "/dev/disk/by-id/ata-ST10000NM0016-1TT101_ZA218QZ0";
       fsType = "ext4";
-      label  = "parity0";
     };
 
     "/mnt/parity1" = {
       device = "/dev/disk/by-id/ata-ST10000NM0016-1TT101_ZA21BXT1";
       fsType = "ext4";
-      label  = "parity1";
     };
 
     "/mnt/data1" = {
       device = "/dev/disk/by-id/ata-WDC_WD40PURX-64NZ6Y0_WD-WCC7K1CZK8JC";
       fsType = "ext4";
-      label  = "data1";
     };
 
     "/mnt/data2" = {
       device = "/dev/disk/by-id/ata-WDC_WD40PURX-64NZ6Y0_WD-WCC7K2JYLLHL";
       fsType = "ext4";
-      label  = "data2";
     };
 
     "/mnt/data3" = {
       device = "/dev/disk/by-id/ata-WDC_WD40PURX-64NZ6Y0_WD-WCC7K2LD2NTR";
       fsType = "ext4";
-      label  = "data3";
     };
 
     "/mnt/data4" = {
       device = "/dev/disk/by-id/ata-WDC_WD40PURX-64NZ6Y0_WD-WCC7K4FEACY1";
       fsType = "ext4";
-      label  = "data4";
     };
 
     "/mnt/data5" = {
       device = "/dev/disk/by-id/ata-WDC_WD40EURX-64WRWY0_WD-WCC4E1745891";
       fsType = "ext4";
-      label  = "data5";
     };
 
     "/mnt/data6" = {
       device = "/dev/disk/by-id/ata-WDC_WD40EURX-64WRWY0_WD-WCC4E2036120";
       fsType = "ext4";
-      label  = "data6";
     };
 
     "/mnt/data7" = {
       device = "/dev/disk/by-id/ata-WDC_WD40EURX-64WRWY0_WD-WCC4E2056001";
       fsType = "ext4";
-      label  = "data7";
     };
 
     "/mnt/data8" = {
       device = "/dev/disk/by-id/ata-HUH721010ALE601_7PK1UM0G";
       fsType = "ext4";
-      label  = "data8";
     };
 
     # PFRD chooses a branch at random with the likelihood of selection based on a branch's
@@ -105,13 +93,15 @@
     securityType = "user";
 
     # [global] Samba section
-    extraConfig = ''
-      browseable  = yes
-      smb encrypt = required
+    settings = {
+      global = {
+        "browseable"  = "yes";
+        "smb encrypt" = "required";
 
-      veto files        = /._*/.DS_Store/
-      delete veto files = yes
-    '';
+        "veto files"        = "/._*/.DS_Store/";
+        "delete veto files" = "yes";
+      };
+    };
 
     shares = {
       Backups = {
